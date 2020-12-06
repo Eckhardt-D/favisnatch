97930ad0e1783e3a833f2f0a6b7884d396c67e24
Eckhardt Dreyer <support@kaizenmedia.co.za>
2020-12-06 10:39:00 +0200

    Fix AR

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,13 @@ export default {
   target: "static",
   head: {
     title: "Favisnatch - Get any website's icon instantly",
+    meta: [
+      {
+        name: "viewport",
+        content:
+          "width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0",
+      },
+    ],
     link: [
       {
         rel: "icon",
